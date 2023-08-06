8b6ff26f9d030d774ef42c9aac3448aad06f5d4b
tarik350 <80385670+tarik350@users.noreply.github.com>
2023-08-06 21:28:41 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Compnents/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <div className="the main container"></div>
+    </div>
+  );
+};
+
+export default App;
